ccad0dc93700b9b69199cf7dabb54164d52143b3
juruo-c <2915601267@qq.com>
2023-10-03 07:34:26 +0800

    23.10.3 partA Task3 Passed

--- src/argmax.s
+++ src/argmax.s
@@ -16,15 +16,31 @@
 # =================================================================
 argmax:
     # Prologue
-
+    li t0 1
+    blt a1 t0 error
+    add t0 a0 zero
+    add t1 a1 zero
+    slli t1 t1 2
+    add t1 a0 t1
+    add a1 a0 zero
+    addi a0 a0 4
 
 loop_start:
-
+    bge a0 t1 loop_end
+    lw t2 0(t0)
+    lw t3 0(a0)
+    bge t2 t3 loop_continue
+    add t0 a0 zero
 
 loop_continue:
-
+    addi a0 a0 4
+    j loop_start
 
 loop_end:
     # Epilogue
-
+    sub t0 t0 a1
+    srli a0 t0 2
     jr ra
+error:
+    li a0 36
+    j exit
